611818f0ef61c355cc00d7ffa48c2e1510820929
hizir <l-n-pe@mail.ru>
2023-06-28 01:42:56 +0300

    added HWTask8 solution

---
+++ seminar1/HWTask8/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 8: Напишите программу, которая на вход принимает число (N), 
+//а на выходе показывает все чётные числа от 1 до N.
+
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+System.Console.WriteLine("введите число");
+
+int N = 0; 
+
+if  (int.TryParse(Console.ReadLine() , out N ) ) 
+{   if (N > 0)
+    {    for ( int n = 1; n <= N ;  n ++ )
+        {
+            if (n % 2 == 0) System.Console.Write(n + " ,");
+        }
+    }
+    else 
+    {
+        for (int n2 = -1; n2 >= N ; n2 --)
+        {
+            if (n2 % 2 == 0) System.Console.Write(n2 + " ,");
+        }
+    }
+
+}
+else 
+{
+System.Console.WriteLine("число не корректно");
+}
