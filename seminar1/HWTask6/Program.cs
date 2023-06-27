a1d39d4282d5558f6245080cb816494a9e2b1cc9
hizir <l-n-pe@mail.ru>
2023-06-28 00:46:21 +0300

    added HWTask6 solution

---
+++ seminar1/HWTask6/Program.cs
@@ -0,0 +1,22 @@
+﻿//Задача 6: Напишите программу, которая на вход принимает число и выдаёт, 
+//является ли число чётным (делится ли оно на два без остатка).
+
+// 4 -> да
+// -3 -> нет
+// 7 -> нет
+
+System.Console.WriteLine("введите число");
+
+int a = 0;
+
+if  (int.TryParse(Console.ReadLine(), out a ) ) //(int.TryParse(Console.ReadLine()) , out a ) 
+{
+    if (a % 2 == 0) 
+    System.Console.WriteLine($"число {a} является четным");
+
+    else System.Console.WriteLine($"число {a} не является четным");
+}
+else
+{
+    System.Console.WriteLine("число не корректно");
+}
